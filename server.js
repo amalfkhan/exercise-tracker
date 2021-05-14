1d0cc612bcbaf473f3d0d1ff027716c19f789de1
Amal Khan <amalfkhan@gmail.com>
2021-05-14 15:59:57 -0700

    add user and exercise logs

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+   useNewUrlParser: true, 
+   useUnifiedTopology: true 
+});
+const Scheme = mongoose.Schema;
+
+const exerciseScheme = new Scheme({
+  description: String,
+  duration: Number,
+  date: Date
+});
+const Exercise = mongoose.model('Exercise', exerciseScheme);
+
+const userScheme = new Scheme({
+  username: String
+});
+const User = mongoose.model('User', userScheme); 
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.route('/api/users')
+.get((req, res) => {
+  User.find({}, (err, data) => {
+    if(err) console.error(err);
+    res.send(data);
+  });
+})
+.post((req, res) => { //creating a new user
+  const newUser = new User({ username: req.body.username });
+  newUser.save((err, data) => {
+    if(err) console.error(err);
+    res.json({
+      _id: data._id,
+      username: data.username
+    });
+  });
+});
+
+//adding an exercise instance to the user's log
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const newExercise = new Exercise({
+    description: req.body.description,
+    duration: req.body.duration,
+    date: req.body.date || new Date()
+  });
+
+  User.findByIdAndUpdate(
+    req.params._id, 
+    { $push: { log: newExercise } },
+    {new: true},
+    (err, data) => {
+      if(err) console.error(err);
+      res.json({
+        username: data.username,
+        description: newExercise.description,
+        duration: newExercise.duration,
+        _id: data._id,
+        date: newExercise.date.toString().split(' ').slice(0,4).join(' ')
+      });
+    }
+  );
+});
+
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+})
